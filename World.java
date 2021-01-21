f748f49859e3f026d101fdf177c43f34d46832af
Wannabe-hub <1213778841@qq.com>
2021-01-22 00:57:49 +0800

    作业

---
+++ World.java
@@ -0,0 +1,15 @@
+package day1;
+/**
+ * 
+ * @author 聂晨
+ *
+ */
+ 
+public class World {
+	//创建类名为World的类
+	public static void main(String[] args) {
+		//使用main的方法
+		System.out.println("goktech!");//输出流控制语句
+	}
+
+}
